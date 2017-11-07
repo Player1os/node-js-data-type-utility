3d464776343e7bc81148167c4f4ca2069c74ac19
Osama Hassanein <player1os@gmail.com>
2017-11-07 17:36:12 +0100

    Remove the underscore from canonic string

--- src/string/token.ts
+++ src/string/token.ts
@@ -4,17 +4,17 @@ import * as Joi from 'joi'
 /**
  * A reg exp that matches all invalid characters in an any string value.
  */
-export const invalidCharactersRegExp = /[^a-z0-9_\-]/g
+export const invalidCharactersRegExp = /[^a-z0-9\-]/g
 
 /**
  * Convert a string value to a token string value.
  * @param value A string value to be converted.
  */
 export const fromString = (value: string) => {
-	return value.toLowerCase().replace(invalidCharactersRegExp, '_')
+	return value.toLowerCase().replace(invalidCharactersRegExp, '-')
 }
 
 /**
  * A validation schema for token string values.
  */
-export const validationSchema = Joi.string().regex(/^[a-z0-9_\-]+$/, 'TokenString').max(256)
+export const validationSchema = Joi.string().regex(/^[a-z0-9\-]+$/, 'TokenString').max(256)
