07bf02f99d5057ca91669e825c59298d473acf67
Osama Hassanein <player1os@gmail.com>
2017-11-17 14:33:14 +0100

    Add valid character reg exp to token string

--- src/string/token.ts
+++ src/string/token.ts
@@ -1,7 +1,17 @@
 /**
+ * A string that can be used to create a regexp for matching token characters.
+ */
+export const validCharacterString = 'a-z0-9\\-'
+
+/**
+ * A string that can be used in a regexp for matching valid token characters.
+ */
+export const validCharacterRegExpString = `[${validCharacterString}]`
+
+/**
  * A string that can be used in a regexp for matching invalid token characters.
  */
-export const invalidCharacterRegExpString = '[^a-z0-9\\-]'
+export const invalidCharacterRegExpString = `[^${validCharacterString}]`
 
 /**
  * A reg exp that matches all invalid characters in an any string value.
